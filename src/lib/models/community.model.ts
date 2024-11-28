27995e55381a1ae402fe368563398ab14b984f1d
Pizle0210 <fajobiadedoyin@hotmail.com>
2024-11-28 19:48:04 +0100

    clerk webhook,community actions and model completed

---
+++ src/lib/models/community.model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { string } from "zod";
+
+const communitySchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  }
+});
+
+const Community =
+  mongoose.models.Community || mongoose.model("Community", communitySchema);
+export { Community };
